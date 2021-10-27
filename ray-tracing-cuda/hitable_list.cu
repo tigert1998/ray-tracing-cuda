50cbd7329206a697d2f29067a7ed1ad0ba76f1b6
tigertang <tigertang.zju@qq.com>
2021-10-27 23:07:12 +0800

    Optimize HitableList::Hit

--- ray-tracing-cuda/hitable_list.cu
+++ ray-tracing-cuda/hitable_list.cu
@@ -6,16 +6,17 @@ using std::pair;
 
 __device__ bool HitableList::Hit(const Ray &ray, double t_from, double t_to,
                                  HitRecord *out) {
-  HitRecord hit_record;
   bool ok = false;
+  HitRecord hit_record;
   for (int i = 0; i < list_len_; i++) {
-    auto ret = list_[i]->Hit(ray, t_from, t_to, &hit_record);
-    if (!ret) continue;
-    if (!ok) {
-      *out = hit_record;
-      ok = true;
-    } else {
-      if (out->t > hit_record.t) {
+    bool ret = list_[i]->Hit(ray, t_from, t_to, &hit_record);
+    if (ret) {
+      if (!ok) {
+        *out = hit_record;
+        t_to = min(t_to, hit_record.t);
+        ok = true;
+      } else if (hit_record.t < t_to) {
+        t_to = hit_record.t;
         *out = hit_record;
       }
     }
